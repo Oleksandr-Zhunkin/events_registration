8caa47268667495c1677ceb52c54213c00652800
Oleksandr Zhynkin <godinsighted@gmail.com>
2024-09-23 14:29:28 +0300

    Update FormRegistration.tsx

--- src/components/FormRegistration/FormRegistration.tsx
+++ src/components/FormRegistration/FormRegistration.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { useForm } from "react-hook-form";
 import s from "./FormRegistration.module.scss";
 import { useNavigate, useParams } from "react-router-dom";
